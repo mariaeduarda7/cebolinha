0616a43c3f0646fe7b38aaea286624baefba9d3c
maria eduarda <duda71771@gmail.com>
2020-03-13 20:11:52 -0300

    exercicio

---
+++ exercicio/Program.cs
@@ -0,0 +1,16 @@
+﻿//Solicite que o usuário digite uma frase. Exiba a frase como se ela fosse dita pelo Cebolinha, da Turma da Mônica.
+
+using System;
+
+namespace exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite uma frase");
+            string  Cebolinha = Console.ReadLine().Replace("r", "l");
+            Console.WriteLine(Cebolinha);
+        }
+    }
+}
